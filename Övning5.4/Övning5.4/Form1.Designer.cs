9bc07adf5593d52e3e839840ebde812b6f32666e
Krille <chrris0223@skynet.halmstad.se>
2019-09-09 14:05:53 +0200

    övning 5.4

---
+++ Övning5.4/Övning5.4/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Övning5._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbxa = new System.Windows.Forms.TextBox();
+            this.txtbxb = new System.Windows.Forms.TextBox();
+            this.txtbxc = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtbxa
+            // 
+            this.txtbxa.Location = new System.Drawing.Point(368, 66);
+            this.txtbxa.Name = "txtbxa";
+            this.txtbxa.Size = new System.Drawing.Size(100, 20);
+            this.txtbxa.TabIndex = 0;
+            // 
+            // txtbxb
+            // 
+            this.txtbxb.Location = new System.Drawing.Point(368, 134);
+            this.txtbxb.Name = "txtbxb";
+            this.txtbxb.Size = new System.Drawing.Size(100, 20);
+            this.txtbxb.TabIndex = 1;
+            // 
+            // txtbxc
+            // 
+            this.txtbxc.Location = new System.Drawing.Point(368, 201);
+            this.txtbxc.Name = "txtbxc";
+            this.txtbxc.Size = new System.Drawing.Size(100, 20);
+            this.txtbxc.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(378, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(378, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtbxc);
+            this.Controls.Add(this.txtbxb);
+            this.Controls.Add(this.txtbxa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbxa;
+        private System.Windows.Forms.TextBox txtbxb;
+        private System.Windows.Forms.TextBox txtbxc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Övning5.4/Övning5.4/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning5._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtbxa.Text);
+            int b = int.Parse(txtbxb.Text);
+            int c = int.Parse(txtbxc.Text);
+
+            bool ordning;
+
+            bool ab = a < b;
+            bool bc = b < c;
+
+            ordning = ab && bc;
+            label1.Text = "Talen är i ordning: " + ordning.ToString();
+        }
+    }
+}
