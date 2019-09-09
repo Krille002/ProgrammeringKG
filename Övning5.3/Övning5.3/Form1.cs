0301a0e3c4a85d3cc6ae87cee73ecab456d61cf8
Krille <chrris0223@skynet.halmstad.se>
2019-09-09 13:57:20 +0200

    5.3

---
+++ Övning5.3/Övning5.3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning5._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int pengar = int.Parse(minapengar.Text);
+            int cost = int.Parse(kostnad.Text);
+
+            bool jagHarRåd = cost <= pengar;
+            label1.Text = "Jag har råd? " + jagHarRåd.ToString();
+        }
+    }
+}
