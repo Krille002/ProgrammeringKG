818e46102bc24d61a36eb56b61464a3d57694e7b
Kicke02 <33360831+Kicke02@users.noreply.github.com>
2019-09-11 21:56:06 +0200

    Uppgift 5.4. Sekudner till timmar, minuter, sekunder

---
+++ Uppgift5.4/Uppgift5.4/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift5._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            int sekunder = int.Parse(textBox1.Text);
+            int timmar = sekunder / (60 * 60);
+            int minuter = (sekunder % (60 * 60)) / 60;
+            int sekunderOver = (sekunder % (60 * 60)) % 60;
+
+            label1.Text = "Timmar: " + timmar;
+            label2.Text = "Minuter: " + minuter;
+            label3.Text = "Sekunder: " + sekunderOver;
+        }
+    }
+}
