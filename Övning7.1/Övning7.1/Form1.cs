0c494edcbdc62fe07ca292d2c7aa4b88423bcf32
Krille <chrris0223@skynet.halmstad.se>
2019-09-16 14:22:23 +0200

    Lektion 1, Kap 7

---
+++ Övning7.1/Övning7.1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning7._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            float tank = float.Parse(textBox1.Text);
+
+            if (tank < 10)
+            {
+                double bensin = 50 - tank;
+                double kostnad = bensin * 14.50;
+                label1.Text = "Tanka " + bensin + "L. Det kostar " + kostnad + "kr.";
+            }
+            else
+            {
+                label1.Text = "Niggu not hungery";
+            }
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+    }
+}
