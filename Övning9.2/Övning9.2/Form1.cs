862ead870caca7a1653516d47df47060ced7f82a
Kicke02 <33360831+Kicke02@users.noreply.github.com>
2019-09-24 22:21:44 +0200

    Övning 9.2 Schack Sädeskorn

---
+++ Övning9.2/Övning9.2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning9._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            int räknare = 0;
+            double korn = 0.5;
+
+            //Loop kvadrera tal
+
+            while (räknare < 64)
+            {
+
+                räknare++;
+                korn = korn * 2;
+
+                textBox1.AppendText("Ruta " + räknare + ":" + "\t" + korn + "\r\n");
+            }
+        }
+    }
+}
