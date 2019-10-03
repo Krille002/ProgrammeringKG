ca67cc40c86a8e4181109beb35996c112fb1ba95
Krille <chrris0223@skynet.halmstad.se>
2019-10-03 13:46:32 +0200

    Progress Övning 9.10

---
+++ Övning9.9/Övning9.9/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning9._9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string[] namnlista = new string[3];
+
+            namnlista[0] = textBox1.Text;
+            namnlista[1] = textBox2.Text;
+            namnlista[2] = textBox3.Text;
+
+            textBox1.Text = namnlista[2];
+            textBox2.Text = namnlista[0];
+            textBox3.Text = namnlista[1];
+        }
+    }
+}
