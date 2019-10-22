342b86ffd01c1a01ca47c737d7debf485158ebe1
Kicke02 <33360831+Kicke02@users.noreply.github.com>
2019-10-22 22:26:27 +0200

    Inför prov. Försöka kolla sista två siffror

---
+++ Test (Select last two digits)/Test (Select last two digits)/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Test__Select_last_two_digits_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxTal = new System.Windows.Forms.TextBox();
+            this.lblTal_No_Touch = new System.Windows.Forms.Label();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxTal
+            // 
+            this.tbxTal.Location = new System.Drawing.Point(117, 35);
+            this.tbxTal.Name = "tbxTal";
+            this.tbxTal.Size = new System.Drawing.Size(100, 20);
+            this.tbxTal.TabIndex = 0;
+            //this.tbxTal.TextChanged += new System.EventHandler(this.TbxTal_TextChanged);
+            // 
+            // lblTal_No_Touch
+            // 
+            this.lblTal_No_Touch.AutoSize = true;
+            this.lblTal_No_Touch.Location = new System.Drawing.Point(74, 38);
+            this.lblTal_No_Touch.Name = "lblTal_No_Touch";
+            this.lblTal_No_Touch.Size = new System.Drawing.Size(25, 13);
+            this.lblTal_No_Touch.TabIndex = 1;
+            this.lblTal_No_Touch.Text = "Tal:";
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSvar.Location = new System.Drawing.Point(77, 81);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(2, 15);
+            this.lblSvar.TabIndex = 2;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(117, 140);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(100, 23);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Testa";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.lblTal_No_Touch);
+            this.Controls.Add(this.tbxTal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxTal;
+        private System.Windows.Forms.Label lblTal_No_Touch;
+        private System.Windows.Forms.Label lblSvar;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ Test (Select last two digits)/Test (Select last two digits)/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test__Select_last_two_digits_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            double tal = double.Parse(tbxTal.Text);
+
+            //Double to string. And length check
+
+            int langd = tal.ToString().Length;
+
+            //Check last two digits
+
+            int sista = tal.ToString()[langd - 1];
+            int nast_sista = tal.ToString()[langd - 2];
+
+            //check if last two digits are the same number and print result
+
+            if (sista == nast_sista)
+            {
+                lblSvar.Text = "De två sista siffrorna i talet \"" + tal + "\" är lika";
+            }
+            else
+            {
+                lblSvar.Text = "De två sista siffrorna i talet \"" + tal + "\" är inte lika";
+            }
+        }
+    }
+}
