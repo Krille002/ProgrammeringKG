a0320c3f3270fd35cdaa0428efd2095d283f3e48
Kicke02 <33360831+Kicke02@users.noreply.github.com>
2019-10-29 23:16:19 +0100

    Outparametrar

---
+++ Övning10.6/Övning10.6/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Övning10._6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxRadie = new System.Windows.Forms.TextBox();
+            this.tbxHojd = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Radie";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Höjd";
+            // 
+            // tbxRadie
+            // 
+            this.tbxRadie.Location = new System.Drawing.Point(167, 46);
+            this.tbxRadie.Name = "tbxRadie";
+            this.tbxRadie.Size = new System.Drawing.Size(100, 20);
+            this.tbxRadie.TabIndex = 2;
+            // 
+            // tbxHojd
+            // 
+            this.tbxHojd.Location = new System.Drawing.Point(167, 88);
+            this.tbxHojd.Name = "tbxHojd";
+            this.tbxHojd.Size = new System.Drawing.Size(100, 20);
+            this.tbxHojd.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Volym";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSvar.Location = new System.Drawing.Point(164, 141);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(2, 15);
+            this.lblSvar.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 223);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbxHojd);
+            this.Controls.Add(this.tbxRadie);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxRadie;
+        private System.Windows.Forms.TextBox tbxHojd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblSvar;
+    }
+}
+
---
+++ Övning10.6/Övning10.6/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning10._6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Skapa variablar för radie och höjd
+            double radie;
+            double hojd;
+
+            //Anropa "lasIn" metoden
+            lasIn(out radie, out hojd);
+
+            //Svaret från metoden "volym" sparas i variblen "svar" och printas
+            double svar = volym(radie, hojd);
+            lblSvar.Text = svar + "";
+
+
+            //Metod "lasIn" som använder Outparametrar för att läsa in värden och spara dem i "radie" och "höjd"
+            void lasIn(out double r, out double h)
+            {
+                r = double.Parse(tbxRadie.Text);
+                h = double.Parse(tbxHojd.Text);
+
+            }
+
+
+            //Metod "volym" som tar variablarna "radie" och "höjd" och returnerar deras volym
+            double volym(double radius, double height)
+            {
+                return Math.PI * Math.Pow(radius, 2) * height;
+            }
+
+        }
+    }
+}
