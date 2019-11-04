1db7385c4941d6933d6e70e76b7e837ba3b1a36d
Krille <chrris0223@skynet.halmstad.se>
2019-11-04 14:25:11 +0100

    Temp sten sax påse

---
+++ Uppgift8.3/Uppgift8.3/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Uppgift8._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxStenSaxPåse = new System.Windows.Forms.GroupBox();
+            this.gbxMinaResultat = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDatornsVal = new System.Windows.Forms.Label();
+            this.lblResultat = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gbxStenSaxPåse.SuspendLayout();
+            this.gbxMinaResultat.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxStenSaxPåse
+            // 
+            this.gbxStenSaxPåse.Controls.Add(this.label2);
+            this.gbxStenSaxPåse.Controls.Add(this.lblResultat);
+            this.gbxStenSaxPåse.Controls.Add(this.lblDatornsVal);
+            this.gbxStenSaxPåse.Controls.Add(this.label1);
+            this.gbxStenSaxPåse.Controls.Add(this.pictureBox3);
+            this.gbxStenSaxPåse.Controls.Add(this.pictureBox2);
+            this.gbxStenSaxPåse.Controls.Add(this.pictureBox1);
+            this.gbxStenSaxPåse.Location = new System.Drawing.Point(28, 35);
+            this.gbxStenSaxPåse.Name = "gbxStenSaxPåse";
+            this.gbxStenSaxPåse.Size = new System.Drawing.Size(650, 254);
+            this.gbxStenSaxPåse.TabIndex = 0;
+            this.gbxStenSaxPåse.TabStop = false;
+            this.gbxStenSaxPåse.Text = "Sten, sax eller påse";
+            // 
+            // gbxMinaResultat
+            // 
+            this.gbxMinaResultat.Controls.Add(this.textBox1);
+            this.gbxMinaResultat.Location = new System.Drawing.Point(724, 108);
+            this.gbxMinaResultat.Name = "gbxMinaResultat";
+            this.gbxMinaResultat.Size = new System.Drawing.Size(227, 181);
+            this.gbxMinaResultat.TabIndex = 1;
+            this.gbxMinaResultat.TabStop = false;
+            this.gbxMinaResultat.Text = "Mina Resultat";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 16);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(221, 162);
+            this.textBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(27, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(183, 73);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(342, 73);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Datorn valde:";
+            // 
+            // lblDatornsVal
+            // 
+            this.lblDatornsVal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDatornsVal.Location = new System.Drawing.Point(121, 170);
+            this.lblDatornsVal.Name = "lblDatornsVal";
+            this.lblDatornsVal.Size = new System.Drawing.Size(52, 23);
+            this.lblDatornsVal.TabIndex = 4;
+            // 
+            // lblResultat
+            // 
+            this.lblResultat.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultat.Location = new System.Drawing.Point(27, 209);
+            this.lblResultat.Name = "lblResultat";
+            this.lblResultat.Size = new System.Drawing.Size(385, 21);
+            this.lblResultat.TabIndex = 5;
+            this.lblResultat.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(247, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Välj sten, sax eller påse genom att klicka på en bild";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 310);
+            this.Controls.Add(this.gbxMinaResultat);
+            this.Controls.Add(this.gbxStenSaxPåse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbxStenSaxPåse.ResumeLayout(false);
+            this.gbxStenSaxPåse.PerformLayout();
+            this.gbxMinaResultat.ResumeLayout(false);
+            this.gbxMinaResultat.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxStenSaxPåse;
+        private System.Windows.Forms.GroupBox gbxMinaResultat;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblResultat;
+        private System.Windows.Forms.Label lblDatornsVal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
