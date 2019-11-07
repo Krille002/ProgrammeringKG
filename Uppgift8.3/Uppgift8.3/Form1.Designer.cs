434ce2cc93db84b01ba1723802b7124014255200
Krille <chrris0223@skynet.halmstad.se>
2019-11-07 13:42:28 +0100

    Sten, Sax, Påse

--- Uppgift8.3/Uppgift8.3/Form1.Designer.cs
+++ Uppgift8.3/Uppgift8.3/Form1.Designer.cs
@@ -28,21 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.gbxStenSaxPåse = new System.Windows.Forms.GroupBox();
-            this.gbxMinaResultat = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblDatornsVal = new System.Windows.Forms.Label();
-            this.lblResultat = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.lblResultat = new System.Windows.Forms.Label();
+            this.lblDatornsVal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbxPase = new System.Windows.Forms.PictureBox();
+            this.pbxSax = new System.Windows.Forms.PictureBox();
+            this.pbxSten = new System.Windows.Forms.PictureBox();
+            this.gbxMinaResultat = new System.Windows.Forms.GroupBox();
+            this.tbxMinaResultat = new System.Windows.Forms.TextBox();
+            this.btnSpela = new System.Windows.Forms.Button();
             this.gbxStenSaxPåse.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPase)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSten)).BeginInit();
             this.gbxMinaResultat.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // gbxStenSaxPåse
@@ -51,85 +53,16 @@
             this.gbxStenSaxPåse.Controls.Add(this.lblResultat);
             this.gbxStenSaxPåse.Controls.Add(this.lblDatornsVal);
             this.gbxStenSaxPåse.Controls.Add(this.label1);
-            this.gbxStenSaxPåse.Controls.Add(this.pictureBox3);
-            this.gbxStenSaxPåse.Controls.Add(this.pictureBox2);
-            this.gbxStenSaxPåse.Controls.Add(this.pictureBox1);
+            this.gbxStenSaxPåse.Controls.Add(this.pbxPase);
+            this.gbxStenSaxPåse.Controls.Add(this.pbxSax);
+            this.gbxStenSaxPåse.Controls.Add(this.pbxSten);
+            this.gbxStenSaxPåse.Enabled = false;
             this.gbxStenSaxPåse.Location = new System.Drawing.Point(28, 35);
             this.gbxStenSaxPåse.Name = "gbxStenSaxPåse";
             this.gbxStenSaxPåse.Size = new System.Drawing.Size(650, 254);
             this.gbxStenSaxPåse.TabIndex = 0;
             this.gbxStenSaxPåse.TabStop = false;
             this.gbxStenSaxPåse.Text = "Sten, sax eller påse";
-            // 
-            // gbxMinaResultat
-            // 
-            this.gbxMinaResultat.Controls.Add(this.textBox1);
-            this.gbxMinaResultat.Location = new System.Drawing.Point(724, 108);
-            this.gbxMinaResultat.Name = "gbxMinaResultat";
-            this.gbxMinaResultat.Size = new System.Drawing.Size(227, 181);
-            this.gbxMinaResultat.TabIndex = 1;
-            this.gbxMinaResultat.TabStop = false;
-            this.gbxMinaResultat.Text = "Mina Resultat";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(3, 16);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(221, 162);
-            this.textBox1.TabIndex = 0;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(27, 73);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(70, 70);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(183, 73);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(70, 70);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Location = new System.Drawing.Point(342, 73);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(70, 70);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 171);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Datorn valde:";
-            // 
-            // lblDatornsVal
-            // 
-            this.lblDatornsVal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblDatornsVal.Location = new System.Drawing.Point(121, 170);
-            this.lblDatornsVal.Name = "lblDatornsVal";
-            this.lblDatornsVal.Size = new System.Drawing.Size(52, 23);
-            this.lblDatornsVal.TabIndex = 4;
-            // 
-            // lblResultat
-            // 
-            this.lblResultat.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblResultat.Location = new System.Drawing.Point(27, 209);
-            this.lblResultat.Name = "lblResultat";
-            this.lblResultat.Size = new System.Drawing.Size(385, 21);
-            this.lblResultat.TabIndex = 5;
-            this.lblResultat.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label2
             // 
@@ -140,22 +73,112 @@
             this.label2.TabIndex = 6;
             this.label2.Text = "Välj sten, sax eller påse genom att klicka på en bild";
             // 
+            // lblResultat
+            // 
+            this.lblResultat.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultat.Location = new System.Drawing.Point(27, 209);
+            this.lblResultat.Name = "lblResultat";
+            this.lblResultat.Size = new System.Drawing.Size(385, 21);
+            this.lblResultat.TabIndex = 5;
+            this.lblResultat.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblDatornsVal
+            // 
+            this.lblDatornsVal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDatornsVal.Location = new System.Drawing.Point(121, 170);
+            this.lblDatornsVal.Name = "lblDatornsVal";
+            this.lblDatornsVal.Size = new System.Drawing.Size(52, 23);
+            this.lblDatornsVal.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Datorn valde:";
+            // 
+            // pbxPase
+            // 
+            this.pbxPase.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbxPase.BackgroundImage")));
+            this.pbxPase.Location = new System.Drawing.Point(342, 73);
+            this.pbxPase.Name = "pbxPase";
+            this.pbxPase.Size = new System.Drawing.Size(70, 70);
+            this.pbxPase.TabIndex = 2;
+            this.pbxPase.TabStop = false;
+            this.pbxPase.Click += new System.EventHandler(this.pbxPase_Click);
+            // 
+            // pbxSax
+            // 
+            this.pbxSax.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbxSax.BackgroundImage")));
+            this.pbxSax.Location = new System.Drawing.Point(183, 73);
+            this.pbxSax.Name = "pbxSax";
+            this.pbxSax.Size = new System.Drawing.Size(70, 70);
+            this.pbxSax.TabIndex = 1;
+            this.pbxSax.TabStop = false;
+            this.pbxSax.Click += new System.EventHandler(this.pbxSax_Click);
+            // 
+            // pbxSten
+            // 
+            this.pbxSten.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbxSten.BackgroundImage")));
+            this.pbxSten.Location = new System.Drawing.Point(27, 73);
+            this.pbxSten.Name = "pbxSten";
+            this.pbxSten.Size = new System.Drawing.Size(70, 70);
+            this.pbxSten.TabIndex = 0;
+            this.pbxSten.TabStop = false;
+            this.pbxSten.Click += new System.EventHandler(this.pbxSten_Click);
+            // 
+            // gbxMinaResultat
+            // 
+            this.gbxMinaResultat.Controls.Add(this.tbxMinaResultat);
+            this.gbxMinaResultat.Enabled = false;
+            this.gbxMinaResultat.Location = new System.Drawing.Point(724, 108);
+            this.gbxMinaResultat.Name = "gbxMinaResultat";
+            this.gbxMinaResultat.Size = new System.Drawing.Size(227, 181);
+            this.gbxMinaResultat.TabIndex = 1;
+            this.gbxMinaResultat.TabStop = false;
+            this.gbxMinaResultat.Text = "Mina Resultat";
+            // 
+            // tbxMinaResultat
+            // 
+            this.tbxMinaResultat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbxMinaResultat.Location = new System.Drawing.Point(3, 16);
+            this.tbxMinaResultat.Multiline = true;
+            this.tbxMinaResultat.Name = "tbxMinaResultat";
+            this.tbxMinaResultat.ReadOnly = true;
+            this.tbxMinaResultat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxMinaResultat.Size = new System.Drawing.Size(221, 162);
+            this.tbxMinaResultat.TabIndex = 0;
+            // 
+            // btnSpela
+            // 
+            this.btnSpela.Location = new System.Drawing.Point(727, 48);
+            this.btnSpela.Name = "btnSpela";
+            this.btnSpela.Size = new System.Drawing.Size(221, 23);
+            this.btnSpela.TabIndex = 2;
+            this.btnSpela.Text = "Spela";
+            this.btnSpela.UseVisualStyleBackColor = true;
+            this.btnSpela.Click += new System.EventHandler(this.btnSpela_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(976, 310);
+            this.Controls.Add(this.btnSpela);
             this.Controls.Add(this.gbxMinaResultat);
             this.Controls.Add(this.gbxStenSaxPåse);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.gbxStenSaxPåse.ResumeLayout(false);
             this.gbxStenSaxPåse.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPase)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSten)).EndInit();
             this.gbxMinaResultat.ResumeLayout(false);
             this.gbxMinaResultat.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -164,14 +187,15 @@
 
         private System.Windows.Forms.GroupBox gbxStenSaxPåse;
         private System.Windows.Forms.GroupBox gbxMinaResultat;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbxMinaResultat;
         private System.Windows.Forms.Label lblResultat;
         private System.Windows.Forms.Label lblDatornsVal;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbxPase;
+        private System.Windows.Forms.PictureBox pbxSax;
+        private System.Windows.Forms.PictureBox pbxSten;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSpela;
     }
 }
 
--- Uppgift8.3/Uppgift8.3/Form1.cs
+++ Uppgift8.3/Uppgift8.3/Form1.cs
@@ -12,9 +12,90 @@ namespace Uppgift8._3
 {
     public partial class Form1 : Form
     {
+        //medlemsvariabler
+        int nrDatorVinster;
+        int nrSpelarVinster;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSpela_Click(object sender, EventArgs e)
+        {
+            gbxStenSaxPåse.Enabled = true;
+            gbxMinaResultat.Enabled = true;
+            btnSpela.Enabled = false;
+
+            lblDatornsVal.Text = "";
+            lblResultat.Text = "";
+
+            nrDatorVinster = 0;
+            nrSpelarVinster = 0;
+
+        }
+
+        private void pbxSten_Click(object sender, EventArgs e)
+        {
+
+            //Generera datorns val som int
+            Random slump = new Random();
+            int datornsTal = slump.Next(1, 4);
+
+            //Översätta datorns val till sten, sax eller påse
+            string datornsVal = "";
+            switch (datornsTal)
+            {
+                case 1:
+                    datornsVal = "Sten";
+                    break;
+                case 2:
+                    datornsVal = "Sax";
+                    break;
+                case 3:
+                    datornsVal = "Påse";
+                    break;
+            }
+
+            lblDatornsVal.Text = datornsVal;
+
+            //Välja vinnare
+            if (datornsVal.Equals("Sax"))
+            {
+                nrSpelarVinster++;
+            }
+            else if (datornsVal.Equals("Påse"))
+            {
+                nrDatorVinster++;
+            }
+
+            //Visa rundans resultat
+            lblResultat.Text = "Dator: " + nrDatorVinster + "  Spelare: " + nrSpelarVinster;
+
+
+            //Kolla om någon har 3 poäng
+            if (nrDatorVinster > 2 || nrSpelarVinster > 2)
+            {
+                gbxStenSaxPåse.Enabled = false;
+                btnSpela.Enabled = true;
+                tbxMinaResultat.AppendText("Dator: " + nrDatorVinster + "  Spelare " + nrSpelarVinster + "\r\n");
+            }
+
+        }
+
+        private void pbxSax_Click(object sender, EventArgs e)
+        {
+            ///Pallar inte fixa för andra två knapparna. Nästan samma kod
+        }
+
+        private void pbxPase_Click(object sender, EventArgs e)
+        {
+            ///Pallar inte fixa för andra två knapparna. Nästan samma kod
         }
     }
 }
