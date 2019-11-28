0fe0fd2de30097d1fdd0bbc1dadb8e249987fb75
Kicke02 <33360831+Kicke02@users.noreply.github.com>
2019-11-28 20:57:59 +0100

    11.5 klar

---
+++ Övning11.5/Övning11.5/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning11._5
+{
+    public partial class Form1 : Form
+    {
+        Maltavla mal;
+        Sikte sikte;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Starta klasser
+
+            mal = new Maltavla(ClientSize.Width / 2, ClientSize.Height / 2, 60);
+            sikte = new Sikte(ClientSize.Width / 2, ClientSize.Height / 2);
+
+            
+        }
+
+        //Ge klasser tillgång till att rita till skärm
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            mal.Rita(e.Graphics);
+            sikte.Rita(e.Graphics);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            sikte.X = e.X;
+            sikte.Y = e.Y;
+            Invalidate();
+        }
+    }
+}
---
+++ Övning11.5/Övning11.5/Sikte.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Övning11._5
+{
+    class Sikte
+    {
+        //Skapa medlemsvariabler
+        private int _x;
+        private int _y;
+        private int radie = 30;
+
+        //konstruktor för att sätta första värden
+
+        public Sikte (int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        ///Egenskaper för att flytta sikte
+        //Flytta x-led
+        public int X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+
+        //Flytta y-led
+
+        public int Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+
+
+        //Metod för att rita siktet till skärmen
+
+        public void Rita(Graphics g)
+        {
+            Pen sikteKantFarg = new Pen(Color.Green, 3);
+            Pen sikteFarg = new Pen(Color.Green, 1);
+
+            g.DrawEllipse(sikteKantFarg, _x - radie, _y - radie, radie * 2, radie * 2);
+            g.DrawLine(sikteFarg, _x, _y - radie, _x, _y + radie);
+            g.DrawLine(sikteFarg, _x - radie, _y, _x + radie, _y);
+        }
+    }
+}
