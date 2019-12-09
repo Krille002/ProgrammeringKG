40d358f20986c7036a61e7ce17e2157138f26f11
krille <chrris0223@skynet.halmstad.se>
2019-12-09 14:29:01 +0100

    Övning12.1

---
+++ Övning12.1/Övning12.1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Övning12._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTal1 = new System.Windows.Forms.Label();
+            this.lblTal2 = new System.Windows.Forms.Label();
+            this.lblTal3 = new System.Windows.Forms.Label();
+            this.lblTal4 = new System.Windows.Forms.Label();
+            this.lblTal5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btnSortera = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTal1
+            // 
+            this.lblTal1.AutoSize = true;
+            this.lblTal1.Location = new System.Drawing.Point(81, 65);
+            this.lblTal1.Name = "lblTal1";
+            this.lblTal1.Size = new System.Drawing.Size(31, 13);
+            this.lblTal1.TabIndex = 0;
+            this.lblTal1.Text = "Tal 1";
+            // 
+            // lblTal2
+            // 
+            this.lblTal2.AutoSize = true;
+            this.lblTal2.Location = new System.Drawing.Point(81, 120);
+            this.lblTal2.Name = "lblTal2";
+            this.lblTal2.Size = new System.Drawing.Size(31, 13);
+            this.lblTal2.TabIndex = 1;
+            this.lblTal2.Text = "Tal 2";
+            // 
+            // lblTal3
+            // 
+            this.lblTal3.AutoSize = true;
+            this.lblTal3.Location = new System.Drawing.Point(81, 172);
+            this.lblTal3.Name = "lblTal3";
+            this.lblTal3.Size = new System.Drawing.Size(31, 13);
+            this.lblTal3.TabIndex = 2;
+            this.lblTal3.Text = "Tal 3";
+            // 
+            // lblTal4
+            // 
+            this.lblTal4.AutoSize = true;
+            this.lblTal4.Location = new System.Drawing.Point(81, 230);
+            this.lblTal4.Name = "lblTal4";
+            this.lblTal4.Size = new System.Drawing.Size(31, 13);
+            this.lblTal4.TabIndex = 3;
+            this.lblTal4.Text = "Tal 4";
+            // 
+            // lblTal5
+            // 
+            this.lblTal5.AutoSize = true;
+            this.lblTal5.Location = new System.Drawing.Point(81, 281);
+            this.lblTal5.Name = "lblTal5";
+            this.lblTal5.Size = new System.Drawing.Size(31, 13);
+            this.lblTal5.TabIndex = 4;
+            this.lblTal5.Text = "Tal 5";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(58, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(163, 169);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(58, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(163, 227);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(58, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(163, 278);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(58, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // btnSortera
+            // 
+            this.btnSortera.Location = new System.Drawing.Point(163, 327);
+            this.btnSortera.Name = "btnSortera";
+            this.btnSortera.Size = new System.Drawing.Size(58, 23);
+            this.btnSortera.TabIndex = 10;
+            this.btnSortera.Text = "Sortera";
+            this.btnSortera.UseVisualStyleBackColor = true;
+            this.btnSortera.Click += new System.EventHandler(this.BtnSortera_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 396);
+            this.Controls.Add(this.btnSortera);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTal5);
+            this.Controls.Add(this.lblTal4);
+            this.Controls.Add(this.lblTal3);
+            this.Controls.Add(this.lblTal2);
+            this.Controls.Add(this.lblTal1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTal1;
+        private System.Windows.Forms.Label lblTal2;
+        private System.Windows.Forms.Label lblTal3;
+        private System.Windows.Forms.Label lblTal4;
+        private System.Windows.Forms.Label lblTal5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button btnSortera;
+    }
+}
+
---
+++ Övning12.1/Övning12.1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning12._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //Starta array av tal
+
+        int[] listaTal = new int[5];
+
+        //integers som behövs vid sorteringen
+
+        int i, n;
+
+        int length = listaTal.Length;        if(length < 2) return;
+
+            //jämförelsetal
+            int jamforelsetal;
+
+        private void BtnSortera_Click(object sender, EventArgs e)
+        {
+            for(n=1; n<length; new++)
+            {
+
+            }
+        }
+    }
+}
