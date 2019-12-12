551298d9d353cadb7008e04e81d40b7c9f457b23
Krille <chrris0223@skynet.halmstad.se>
2019-12-12 13:32:47 +0100

    12.1 klar

--- Övning12.1/Övning12.1/Form1.Designer.cs
+++ Övning12.1/Övning12.1/Form1.Designer.cs
@@ -39,6 +39,11 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.btnSortera = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTal1
@@ -131,11 +136,61 @@
             this.btnSortera.UseVisualStyleBackColor = true;
             this.btnSortera.Click += new System.EventHandler(this.BtnSortera_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(259, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(259, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "label5";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(342, 396);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSortera);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -167,6 +222,11 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button btnSortera;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Övning12.1/Övning12.1/Form1.cs
+++ Övning12.1/Övning12.1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,31 +13,66 @@ namespace Övning12._1
 {
     public partial class Form1 : Form
     {
+        //Medlemsvariabler
+        int[] listaTal;
+
         public Form1()
         {
             InitializeComponent();
+            listaTal = new int[5];
 
         }
-
-        //Starta array av tal
-
-        int[] listaTal = new int[5];
-
-        //integers som behövs vid sorteringen
-
-        int i, n;
-
-        int length = listaTal.Length;        if(length < 2) return;
-
-            //jämförelsetal
-            int jamforelsetal;
+ 
 
         private void BtnSortera_Click(object sender, EventArgs e)
         {
-            for(n=1; n<length; new++)
-            {
 
+            listaTal[0] = int.Parse(textBox1.Text);
+            listaTal[1] = int.Parse(textBox2.Text);
+            listaTal[2] = int.Parse(textBox3.Text);
+            listaTal[3] = int.Parse(textBox4.Text);
+            listaTal[4] = int.Parse(textBox5.Text);
+
+            sorteraArray(listaTal);
+            //Bifoga lista och ankalla metod för sortering
+        }
+
+        //metod för att sortera array
+
+        public void sorteraArray (int[] lista)
+        {
+            int i, n;
+            int length = lista.Length;
+            int jamforelsetal;
+
+            //Är listan under två element lång måste den inte sorteras
+            if (length < 2) return;
+
+
+            for( n=1 ; n < length; n++)
+            {
+                //Spara jämförelsetal
+                jamforelsetal = lista[n];
+
+                //Gå tillbaka ett element
+
+                i = n - 1;
+
+                while(i >= 0 && lista[i] > jamforelsetal)
+                {
+                    lista[i + 1] = lista[i];
+                    i--;
+                }
+
+                lista[i+1] = jamforelsetal;
+               
             }
+
+            label1.Text = lista[0].ToString();
+            label2.Text = lista[1].ToString();
+            label3.Text = lista[2].ToString();
+            label4.Text = lista[3].ToString();
+            label5.Text = lista[4].ToString();
         }
     }
 }
