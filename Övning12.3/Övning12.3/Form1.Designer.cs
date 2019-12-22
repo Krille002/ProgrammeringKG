6f0629f6a9a4ef4e15b782eb49ea0aab63cbdcf4
Kicke02 <33360831+Kicke02@users.noreply.github.com>
2019-12-22 23:53:51 +0100

    övning 12.3 färdig

---
+++ Övning12.3/Övning12.3/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Övning12._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxAntal = new System.Windows.Forms.TextBox();
+            this.tbxSok = new System.Windows.Forms.TextBox();
+            this.btnSortera = new System.Windows.Forms.Button();
+            this.btnBinarsok = new System.Windows.Forms.Button();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGenerera = new System.Windows.Forms.Button();
+            this.btnSok = new System.Windows.Forms.Button();
+            this.tbxLista = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbxAntal
+            // 
+            this.tbxAntal.Location = new System.Drawing.Point(141, 51);
+            this.tbxAntal.Name = "tbxAntal";
+            this.tbxAntal.Size = new System.Drawing.Size(100, 20);
+            this.tbxAntal.TabIndex = 0;
+            // 
+            // tbxSok
+            // 
+            this.tbxSok.Location = new System.Drawing.Point(141, 108);
+            this.tbxSok.Name = "tbxSok";
+            this.tbxSok.Size = new System.Drawing.Size(100, 20);
+            this.tbxSok.TabIndex = 1;
+            // 
+            // btnSortera
+            // 
+            this.btnSortera.Enabled = false;
+            this.btnSortera.Location = new System.Drawing.Point(73, 189);
+            this.btnSortera.Name = "btnSortera";
+            this.btnSortera.Size = new System.Drawing.Size(75, 23);
+            this.btnSortera.TabIndex = 2;
+            this.btnSortera.Text = "Sortera";
+            this.btnSortera.UseVisualStyleBackColor = true;
+            this.btnSortera.Click += new System.EventHandler(this.BtnSortera_Click);
+            // 
+            // btnBinarsok
+            // 
+            this.btnBinarsok.Enabled = false;
+            this.btnBinarsok.Location = new System.Drawing.Point(166, 189);
+            this.btnBinarsok.Name = "btnBinarsok";
+            this.btnBinarsok.Size = new System.Drawing.Size(75, 23);
+            this.btnBinarsok.TabIndex = 3;
+            this.btnBinarsok.Text = "Binärsök";
+            this.btnBinarsok.UseVisualStyleBackColor = true;
+            this.btnBinarsok.Click += new System.EventHandler(this.BtnBinarsok_Click);
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.Location = new System.Drawing.Point(70, 315);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(0, 13);
+            this.lblSvar.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sök";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Antal";
+            // 
+            // btnGenerera
+            // 
+            this.btnGenerera.Location = new System.Drawing.Point(302, 49);
+            this.btnGenerera.Name = "btnGenerera";
+            this.btnGenerera.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerera.TabIndex = 7;
+            this.btnGenerera.Text = "Generera";
+            this.btnGenerera.UseVisualStyleBackColor = true;
+            this.btnGenerera.Click += new System.EventHandler(this.BtnGenerera_Click);
+            // 
+            // btnSok
+            // 
+            this.btnSok.Enabled = false;
+            this.btnSok.Location = new System.Drawing.Point(302, 106);
+            this.btnSok.Name = "btnSok";
+            this.btnSok.Size = new System.Drawing.Size(75, 23);
+            this.btnSok.TabIndex = 8;
+            this.btnSok.Text = "Sök";
+            this.btnSok.UseVisualStyleBackColor = true;
+            this.btnSok.Click += new System.EventHandler(this.BtnSok_Click);
+            // 
+            // tbxLista
+            // 
+            this.tbxLista.Location = new System.Drawing.Point(514, 12);
+            this.tbxLista.Multiline = true;
+            this.tbxLista.Name = "tbxLista";
+            this.tbxLista.ReadOnly = true;
+            this.tbxLista.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxLista.Size = new System.Drawing.Size(168, 417);
+            this.tbxLista.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 441);
+            this.Controls.Add(this.tbxLista);
+            this.Controls.Add(this.btnSok);
+            this.Controls.Add(this.btnGenerera);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.btnBinarsok);
+            this.Controls.Add(this.btnSortera);
+            this.Controls.Add(this.tbxSok);
+            this.Controls.Add(this.tbxAntal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxAntal;
+        private System.Windows.Forms.TextBox tbxSok;
+        private System.Windows.Forms.Button btnSortera;
+        private System.Windows.Forms.Button btnBinarsok;
+        private System.Windows.Forms.Label lblSvar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGenerera;
+        private System.Windows.Forms.Button btnSok;
+        private System.Windows.Forms.TextBox tbxLista;
+    }
+}
+
---
+++ Övning12.3/Övning12.3/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning12._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        //Medlemsvariabler. Kan inte sättas till icke-statiska värden här
+        int[] lista;
+        int sok;
+        int funnetIndex;
+        
+
+        //Generera randoms
+        Random random = new Random();
+
+
+
+        private void BtnGenerera_Click(object sender, EventArgs e)
+        {
+            //antal nummer
+            int antalNummer = int.Parse(tbxAntal.Text);
+
+            try
+            {
+                if (antalNummer <= 10000)
+                {
+                    lista = new int[antalNummer];
+
+                    for (int i = 0; i < antalNummer; i++)
+                    {
+                        int rand = random.Next(1, 10000);
+                        lista[i] = rand;
+                        tbxLista.AppendText(i + ", " + rand + "\r\n");
+                    }
+
+                    btnSok.Enabled = true;
+                    btnSortera.Enabled = true;
+                    btnBinarsok.Enabled = false;
+                }
+                //Ifall if-satsen inte funkar av någon anledning
+                else
+                {
+                    MessageBox.Show("Inmatningsfel", "Fel", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            //Ifall något går väldigt fel
+            catch
+            {
+                MessageBox.Show("Något gick väldigt fel", "Fel", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+
+        private void BtnSok_Click(object sender, EventArgs e)
+        {
+            sok = int.Parse(tbxSok.Text);
+
+            funnetIndex = sekventiellSokning(lista, sok);
+
+            //Om metod inte hittar något index (ger -1)
+            if (funnetIndex == -1) lblSvar.Text = "Numret " + sok + " finns inte i listan";
+
+            //Printa svar annars
+            else lblSvar.Text = "Numret " + sok + " finns på index " + funnetIndex;
+
+
+        }
+
+        private void BtnSortera_Click(object sender, EventArgs e)
+        {
+            bubbelSortera(lista);
+            btnBinarsok.Enabled = true;
+        }
+
+        private void BtnBinarsok_Click(object sender, EventArgs e)
+        {
+            sok = int.Parse(tbxSok.Text);
+            funnetIndex = binarSokning(lista, sok);
+
+            //Om metod inte hittar något index (ger -1)
+            if (funnetIndex == -1) lblSvar.Text = "Numret " + sok + " finns inte i listan";
+
+            //Printa svar annars
+            else lblSvar.Text = "Numret " + sok + " finns på index " + funnetIndex;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //metoder
+
+        //Sekventiell
+        public int sekventiellSokning(int[] metodLista, int värde)
+        {
+            //Returnerar -1 som är omöjligt ifall värde inte finns
+            int index = -1;
+
+            //Sekventiell. Jämför alla element mot värdet manuellt
+            for(int i = 0; i < metodLista.Length; i++)
+            {
+                if(metodLista[i] == värde)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+
+            //Binär
+
+        public int binarSokning(int[] metodLista, int värde)
+        {
+            int index = -1;
+            int min = 0;
+            int max = metodLista.Length - 1;
+
+            while(min <= max && index == -1)
+            {
+                int mitt = (min + max) / 2;
+
+                if (värde > metodLista[mitt])
+                {
+                    min = mitt + 1;
+                }
+                else if (värde < metodLista[mitt])
+                {
+                    max = mitt - 1;
+                }
+                else index = mitt;
+            }
+            return index;
+        }
+
+            //Sortering (bubbel)
+
+        void bubbelSortera(int[] metodLista)
+        {
+            //bestäma hur många nummer sorteringen går igenom. 1 mindre varje runda eftersom att sista kommer elementet kommer vara störst
+            for(int i = metodLista.Length-1; i > 0; i--)
+            {
+                //Sortera alla nummer upp till där yttersta for-loopen sätter stopp
+                for(int j = 0; j<i; j++)
+                {
+                    //Om tal bakom större, byt plats
+                    if(metodLista[j] > metodLista[j + 1])
+                    {
+                        int temp = metodLista[j];
+                        metodLista[j] = metodLista[j + 1];
+                        metodLista[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
