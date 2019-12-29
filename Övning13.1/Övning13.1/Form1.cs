596d3e7fb5707adb4dc2eb08a49bbd49c9598478
Kicke02 <33360831+Kicke02@users.noreply.github.com>
2019-12-30 00:35:51 +0100

    Övning 13.1

---
+++ Övning13.1/Övning13.1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning13._1
+{
+    public partial class Form1 : Form
+    {
+
+        List<Rektangel> rektanglar = new List<Rektangel>();
+        bool speletSlut = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Generera rektanglar (4x4 grid)
+
+            for(int rad = 0; rad < 4; rad++)
+            {
+                for(int kolumn = 0; kolumn < 4; kolumn++)
+                {
+                    //Skapa värden
+                    int x = 10 + 100 * rad;
+                    int y = 10 + 100 * kolumn;
+                    int hojd = 50;
+                    int bredd = 75;
+
+                    //Skapa temporärt object, rektangel av värden
+                    Rektangel temporarRektangel = new Rektangel(x, y, hojd, bredd);
+                    //Lägga till temporär rektangel i lista
+                    rektanglar.Add(temporarRektangel);
+                }
+            }
+
+
+        }
+
+
+
+        //Rita till skärm
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            //Rita rektanglar
+            for(int i = 0; i < rektanglar.Count; i++)
+            {
+                //Skapa temporärt objekt, rektangel för att extrahera ur listan
+                Rektangel temporarRektangel2 = rektanglar.ElementAt(i);
+                temporarRektangel2.Rita(g);
+            }
+
+            //Om spelet är slut
+            if (speletSlut)
+            {
+                //Variabler
+                string text = "Grattis!";
+                Font = new Font("Arial", 30);
+                SolidBrush brushTillText = new SolidBrush(Color.Blue);
+
+                //Plats
+                int x = 50;
+                int y = 50;
+
+                //Rita text till skärm
+                g.DrawString(text, Font, brushTillText, x, y);
+            }
+
+        }
+
+
+        //Musklick
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            
+        }
+
+
+    }
+}
---
+++ Övning13.1/Övning13.1/Rektangel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Övning13._1
+{
+    class Rektangel
+    {
+        //Variabler
+        private int x = 0;
+        private int y = 0;
+        private int h = 50;
+        private int b = 75;
+
+
+        //Konstruktor
+        public Rektangel (int X, int Y, int hojd, int bredd)
+        {
+            x = X;
+            y = Y;
+            h = hojd;
+            b = bredd;
+        }
+
+
+        //Egenskaper
+
+            //X-koordinat
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+            //Y-koordinat
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+            //Bredd
+        public int Bredd
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                b = value;
+            }
+        }
+            //Höjd
+        public int Hojd
+        {
+            get
+            {
+                return h;
+            }
+            set
+            {
+                h = value;
+            }
+        }
+
+        //Metoder
+
+               //Rita Rektanglar
+        public void Rita (Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(Color.Red), x, y, b, h);
+
+        }
+
+        public bool Träffad(int musX, int musY)
+        {
+            //Kolla om muspekare är i någon rektangel
+        }
+    }
+}
