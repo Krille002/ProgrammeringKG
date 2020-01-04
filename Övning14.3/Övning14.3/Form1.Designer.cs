055b6f1d43919c23956a49b8d53834e8e9247a04
Krille <33360831+Kicke02@users.noreply.github.com>
2020-01-04 19:15:22 +0100

    Övning 14.3 klar

---
+++ Övning14.3/Övning14.3/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Övning14._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUt = new System.Windows.Forms.Label();
+            this.btnVisaRatt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUt
+            // 
+            this.lblUt.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblUt.Location = new System.Drawing.Point(12, 9);
+            this.lblUt.Name = "lblUt";
+            this.lblUt.Size = new System.Drawing.Size(160, 23);
+            this.lblUt.TabIndex = 0;
+            // 
+            // btnVisaRatt
+            // 
+            this.btnVisaRatt.Location = new System.Drawing.Point(12, 76);
+            this.btnVisaRatt.Name = "btnVisaRatt";
+            this.btnVisaRatt.Size = new System.Drawing.Size(160, 23);
+            this.btnVisaRatt.TabIndex = 1;
+            this.btnVisaRatt.Text = "Visa i rättvänd ordning";
+            this.btnVisaRatt.UseVisualStyleBackColor = true;
+            this.btnVisaRatt.Click += new System.EventHandler(this.BtnVisaRatt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 111);
+            this.Controls.Add(this.btnVisaRatt);
+            this.Controls.Add(this.lblUt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUt;
+        private System.Windows.Forms.Button btnVisaRatt;
+    }
+}
+
---
+++ Övning14.3/Övning14.3/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning14._3
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnVisaRatt_Click(object sender, EventArgs e)
+        {
+            //Skapa fält med värden
+            double[] resultat = { 1.65, 1.60, 1.55, 1.5, 1.45, 1.40 };
+
+            VandRatt(resultat, 0);
+        }
+
+        private void VandRatt(double[] Resultat, int n)
+        {
+ 
+            if (n == 5)
+            {
+                lblUt.Text += Resultat[5] + " ";
+            }
+            else
+            {
+                lblUt.Text += Resultat[n] + " ";
+                VandRatt(Resultat, n + 1);
+            }
+        }
+
+
+    }
+}
