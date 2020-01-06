024850eb9f54f8c2c08e049bbcd45bb7e9e2f2f5
Krille <33360831+Kicke02@users.noreply.github.com>
2020-01-06 22:36:04 +0100

    Övning 14.4 klar

---
+++ Övning14.4/Övning14.4/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning14._4
+{
+    public partial class Form1 : Form
+    {
+
+        private Papper[] papperArray = new Papper[10];
+        private bool ritaPapper = false;
+        int antalStorlekar;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void BtnRita_Click(object sender, EventArgs e)
+        {
+            antalStorlekar = int.Parse(tbxStorlek.Text);
+
+            skapaPapper(0, 20, 50, 200, 300);
+            ritaPapper = true;
+            Invalidate();
+        }
+
+
+
+
+        private void skapaPapper(int n, int x, int y, int bredd, int hojd)
+        {
+            if(n < antalStorlekar)
+            {
+                if (n == 5)
+                {
+                    Papper p = new Papper(x, y, bredd, hojd);
+                    papperArray[n] = p;
+                }
+                else
+                {
+                    Papper p = new Papper(x, y, bredd, hojd);
+                    papperArray[n] = p;
+
+                    if (n % 2 == 0)
+                    {
+                        x = x + bredd;
+                        hojd = hojd / 2;
+                    }
+                    else
+                    {
+                        y = y + hojd;
+                        bredd = bredd / 2;
+                    }
+
+                    n++;
+                    skapaPapper(n, x, y, bredd, hojd);
+
+                }
+            }
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            if (ritaPapper)
+            {
+                //Rita ut alla rektanglar
+                for (int i = 0; i < antalStorlekar; i++)
+                {
+                    papperArray[i].Rita(g);
+                }
+            }
+        }
+    }
+}
---
+++ Övning14.4/Övning14.4/Papper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Övning14._4
+{
+    class Papper
+    {
+
+        private int X;
+        private int Y;
+        private int bredd_;
+        private int hojd_;
+
+        public Papper(int x, int y, int bredd, int hojd)
+        {
+            X = x;
+            Y = y;
+            bredd_ = bredd;
+            hojd_ = hojd;
+        }
+
+        public void Rita(Graphics g)
+        {
+            Pen penna = new Pen(Color.Red);
+            g.DrawRectangle(penna, X, Y, bredd_, hojd_);
+        }
+
+    }
+}
