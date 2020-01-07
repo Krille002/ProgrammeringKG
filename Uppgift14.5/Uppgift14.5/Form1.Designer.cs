26d40ff1338ee09f2dfcb3cbb53b71cd05c069b2
Krille <33360831+Kicke02@users.noreply.github.com>
2020-01-07 13:18:41 +0100

    Uppgift14.5 nästan klar. något fel

---
+++ Uppgift14.5/Uppgift14.5/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Uppgift14._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSok = new System.Windows.Forms.Button();
+            this.tbxSoktTal = new System.Windows.Forms.TextBox();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sökt tal:";
+            // 
+            // btnSok
+            // 
+            this.btnSok.Location = new System.Drawing.Point(122, 61);
+            this.btnSok.Name = "btnSok";
+            this.btnSok.Size = new System.Drawing.Size(75, 23);
+            this.btnSok.TabIndex = 2;
+            this.btnSok.Text = "Sök";
+            this.btnSok.UseVisualStyleBackColor = true;
+            this.btnSok.Click += new System.EventHandler(this.BtnSok_Click);
+            // 
+            // tbxSoktTal
+            // 
+            this.tbxSoktTal.Location = new System.Drawing.Point(65, 63);
+            this.tbxSoktTal.Name = "tbxSoktTal";
+            this.tbxSoktTal.Size = new System.Drawing.Size(51, 20);
+            this.tbxSoktTal.TabIndex = 3;
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSvar.Location = new System.Drawing.Point(12, 123);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(191, 23);
+            this.lblSvar.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(215, 155);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.tbxSoktTal);
+            this.Controls.Add(this.btnSok);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSok;
+        private System.Windows.Forms.TextBox tbxSoktTal;
+        private System.Windows.Forms.Label lblSvar;
+    }
+}
+
---
+++ Uppgift14.5/Uppgift14.5/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgift14._5
+{
+    public partial class Form1 : Form
+    {
+
+        //Skapa array
+        int[] Varden = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+        int svar = -1;
+
+        //Skriva ut array till label1
+        public Form1()
+        {
+            InitializeComponent();
+
+            for(int i = 0; i < Varden.Count(); i++)
+            {
+                label1.Text = label1.Text + Varden[i] + " ";
+            }
+
+            
+        }
+
+        //Sökknapp
+        private void BtnSok_Click(object sender, EventArgs e)
+        {
+            int soktTal_ = int.Parse(tbxSoktTal.Text);
+
+            int initialMax = Varden.Length - 1;
+            int initialMin = 0;
+
+            Hitta(Varden, soktTal_, initialMax, initialMin);
+
+            lblSvar.Text = svar.ToString();
+        }
+
+
+        //Binär sökning
+        private void Hitta(int[] lista, int soktTal, int max, int min)
+        {
+            //Plats på värde är från början odefinerat (-1)
+            //mitt skapas varje gång metoden kallas
+            int plats = -1;
+            int mitt = (max + min) / 2;
+
+            if(mitt == max || mitt == min)
+            {
+                svar = mitt;
+            }
+            else
+            {
+                if(soktTal > lista[mitt])
+                {
+                    min = mitt + 1;
+                    Hitta(lista, soktTal, max, min);
+                }
+                else
+                {
+                    max = mitt - 1;
+                    Hitta(lista, soktTal, max, min);
+                }
+            }
+
+
+
+        }
+    }
+}
