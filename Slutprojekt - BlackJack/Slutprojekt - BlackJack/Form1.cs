4423c98a0835f251df539c73fa6d922c1bc5d61f
Krille <33360831+Kicke02@users.noreply.github.com>
2020-01-19 13:47:40 +0100

    Check card value

--- Slutprojekt - BlackJack/Slutprojekt - BlackJack/Form1.cs
+++ Slutprojekt - BlackJack/Slutprojekt - BlackJack/Form1.cs
@@ -16,6 +16,7 @@ namespace Slutprojekt___BlackJack
         Players currentPlayers = new Players();
         
         int debugInteger = 0;
+        int playerHandValue = 0;
 
         public Form1()
         {
@@ -34,10 +35,11 @@ namespace Slutprojekt___BlackJack
         private void BtnDebug_Click(object sender, EventArgs e)
         {
             if (debugInteger == 52) debugInteger = 0;
-            label1.Text = currentDeck.DebugPrint(debugInteger);
+            //label1.Text = currentDeck.DebugPrint(debugInteger);
             debugInteger++;
 
-            label1.Text = currentPlayers.DealerHand[0];
+            //label1.Text = currentPlayers.DealerHand[0];
+            label1.Text = playerHandValue.ToString();
         }
 
 
@@ -52,9 +54,11 @@ namespace Slutprojekt___BlackJack
             string PulledCard = currentDeck.PullCard();
             currentPlayers.PlayerHandAdd(PulledCard);
 
+            playerHandValue = playerHandValue + CheckCardValue(PulledCard);
             //Räkna ut spelares totala poäng
             //Rita ut kort och poäng till skärm
             //Hantera ess och gå över 21
+
 
 
         }
@@ -69,6 +73,35 @@ namespace Slutprojekt___BlackJack
             //Jämföra spelares och Dealers poäng
             //Bestäm vinnare
 
+        }
+
+
+
+        public int CheckCardValue (string cardString)
+        {
+            int firstNumber;
+            int secondNumber;
+
+            if(cardString.Length == 2)
+            {
+                firstNumber = cardString[0];
+
+                return firstNumber;
+            }
+            else if (cardString.Length == 3)
+            {
+                firstNumber = cardString[0];
+                secondNumber = cardString[1];
+
+                string cardNumberString = firstNumber.ToString() + secondNumber.ToString();
+
+                return int.Parse(cardNumberString);
+            }
+            else
+            {
+                return -1;
+            }
+            
         }
     }
 }
