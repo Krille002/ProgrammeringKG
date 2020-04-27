660e9143723c32d51aefde347a4eb0231874117c
Krille <christian.ristevski@gmail.com>
2020-04-27 22:45:38 +0200

    Snake added

---
+++ Snake/Snake/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.FillRectangle(new SolidBrush(color: Color.Black), 20, 20, this.Width - 55, this.Height - 80);
+
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}
